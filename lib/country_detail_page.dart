ab45cc2e1bb6381b48f910e881f669ca8e0657f8
Will Toozs-Hobson <KazToozs>
2020-03-25 10:44:59 +0100

    Handle missing data in detail page

--- lib/country_detail_page.dart
+++ lib/country_detail_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:corona_app/country_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +7,13 @@ class CountryDetailPage extends StatelessWidget {
   CountryData details;
 
   CountryDetailPage({Key key, @required this.details}) : super(key: key);
+
+  String checkData(String string) {
+    if (string.compareTo("null") == 0) {
+      return ("No data");
+    }
+    return (string);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,24 +26,28 @@ class CountryDetailPage extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(16.0),
-            child: Text("Confirmed Cases: " + details.dailyData.last.confirmed.toString()),
+            child: Text("Confirmed Cases: " +
+                checkData(details.dailyData.last.confirmed.toString())),
           ),
           Padding(
             padding: EdgeInsets.all(16.0),
-            child: Text("Related Deaths: " + details.dailyData.last.deaths.toString()),
+            child: Text("Related Deaths: " +
+                checkData(details.dailyData.last.deaths.toString())),
           ),
           Padding(
             padding: EdgeInsets.all(16.0),
-            child: Text("Recovered: " + details.dailyData.last.recovered.toString()),
+            child: Text("Recovered: " +
+                checkData(details.dailyData.last.recovered.toString())),
           ),
           Center(
             child: Padding(
               padding: EdgeInsets.all(16.0),
-              child: Text("Last updated: " + details.dailyData.last.date.toString()),
+              child: Text("Last updated: " +
+                  checkData(details.dailyData.last.date.toString())),
             ),
           ),
         ],
       ),
-    );  
-    }
+    );
+  }
 }
